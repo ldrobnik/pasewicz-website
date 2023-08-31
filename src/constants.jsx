5bee3c9644925f260d075c8616ec4b689a5a0763
Kacper Jajuga <96070925+KacperJajuga@users.noreply.github.com>
2023-08-31 20:07:25 +0200

    Change constants file extension

---
+++ src/constants.jsx
@@ -0,0 +1,202 @@
+// Section names
+export const SECTIONS = ["Biogram", "Najnowsze", "Publikacje"];
+
+export const BOOKS = [
+  {
+    title: "Darkslide",
+    year: 2022,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/5042000/5042683/1024808-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Pulverkopf",
+    year: 2021,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/157000/157695/1026834-352x500.jpg",
+    publisher: "Wielka Litera",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Sztuka bycia niepotrzebnym",
+    year: 2020,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/4973000/4973564/980439-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Śmierć w Darkroomie. Wydanie rozszerzone",
+    year: 2018,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/4868000/4868656/700902-352x500.jpg",
+    publisher: "Bricolage Publishing",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Miejsce",
+    year: 2016,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/4378000/4378964/979695-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Och, Mitochondria",
+    year: 2015,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/258000/258167/391447-352x500.jpg",
+    publisher: "EMG",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Pałacyk Bertolta Brechta",
+    year: 2011,
+    cover: "https://s.lubimyczytac.pl/upload/books/100000/100753/352x500.jpg",
+    publisher: "EMG",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Muzyka na instrumenty strunowe, perkusję i czelestę",
+    year: 2010,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/80000/80838/980893-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Drobne! Drobne!",
+    year: 2008,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/58000/58215/980892-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Śmierć w darkroomie",
+    year: 2007,
+    cover: "https://s.lubimyczytac.pl/upload/books/27000/27473/352x500.jpg",
+    publisher: "EMG",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Dolna Wilda. Wydanie drugie",
+    year: 2006,
+    cover:
+      "https://s.lubimyczytac.pl/upload/books/58000/58310/980890-352x500.jpg",
+    publisher:
+      "Wojewódzka Biblioteka Publiczna i Centrum Animacji Kultury w Poznaniu",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Henry Berryman Pięśni",
+    year: 2006,
+    cover: "https://s.lubimyczytac.pl/upload/books/166000/166224/352x500.jpg",
+    publisher: "Kserokopia.art.pl",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Wiersze dla Róży Filipowicz",
+    year: 2004,
+    cover: "https://s.lubimyczytac.pl/upload/books/133000/133745/352x500.jpg",
+    publisher: "Biuro Literackie",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+  {
+    title: "Dolna Wilda",
+    year: 2001,
+    cover: "https://readeatslip.files.wordpress.com/2010/06/pasewicz.jpg",
+    publisher: "Fundacja ANIMA, Tygiel Kultury",
+    description: (
+      <>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi in
+        delectus necessitatibus eveniet voluptas <strong>placeat</strong> optio
+        et quam ullam!
+      </>
+    ),
+  },
+];
