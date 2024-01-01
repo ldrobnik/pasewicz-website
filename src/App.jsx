530391451a98c26aa4a25ce953013bee6f7a3f3b
Łukasz Drobnik <drobniklukasz@gmail.com>
2024-01-01 11:28:56 +0100

    Create theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#002e3e",
+    },
+    secondary: {
+      main: "#e04c32",
+    },
+    background: {
+      default: "#ebe3df",
+    },
+  },
+});
