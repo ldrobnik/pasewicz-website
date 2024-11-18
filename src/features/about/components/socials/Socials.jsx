25dd4dd5de8e8ad2779e3d0b7c02078ec24e710d
Łukasz Drobnik <drobniklukasz@gmail.com>
2024-11-18 20:04:42 +0100

    Add styles to Socials

--- src/features/about/components/socials/Socials.jsx
+++ src/features/about/components/socials/Socials.jsx
@@ -1,18 +1,43 @@
 import { SOCIALS } from "../../../../constants";
+import { styled } from "@mui/system";
+import { Button, useMediaQuery } from "@mui/material";
+
+const StyledWrapper = styled("div")(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  padding: "2rem 0",
+}));
+
+const StyledButton = styled(Button)(({ isLargeScreen }) => ({
+  borderRadius: 0,
+  margin: isLargeScreen ? "1rem" : "0.4rem",
+}));
 
 const Socials = () => {
+  // Check for screen width
+  const isLargeScreen = useMediaQuery("(min-width:620px)");
+
   return (
-    <ul>
+    <StyledWrapper>
       {SOCIALS.map((link) => {
         return (
-          <li key={link.name}>
-            <a href={link.url} target="_blank" rel="noopener noreferrer">
-              {link.name}
-            </a>
-          </li>
+          <StyledButton
+            key={link.name}
+            variant="contained"
+            color="primary"
+            size="small"
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            isLargeScreen={isLargeScreen}
+          >
+            {link.name}
+          </StyledButton>
         );
       })}
-    </ul>
+    </StyledWrapper>
   );
 };
 
