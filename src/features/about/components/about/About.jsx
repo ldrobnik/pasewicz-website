266ab1f702ff4cd67fd400b7b6a943129552253e
Łukasz Drobnik <drobniklukasz@gmail.com>
2024-11-18 17:32:54 +0100

    Add styled container to About

--- src/features/about/components/about/About.jsx
+++ src/features/about/components/about/About.jsx
@@ -1,12 +1,31 @@
+import { Container } from "@mui/material";
+import { styled } from "@mui/system";
 import Bio from "../bio/Bio";
 import Socials from "../socials/Socials";
 
+const StyledContainer = styled(Container)(({ theme }) => ({
+  // Add custom selection styling
+  "& ::selection": {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.background.default,
+  },
+}));
+const Spacer = styled("div")(() => ({
+  minHeight: "64 px",
+}));
+
 const About = () => {
   return (
-    <>
+    <StyledContainer
+      maxWidth="md"
+      sx={{
+        marginTop: { xs: "56px", sm: "64px" }, // Shift content below AppBar
+        padding: 2,
+      }}
+    >
       <Bio />
       <Socials />
-    </>
+    </StyledContainer>
   );
 };
 
